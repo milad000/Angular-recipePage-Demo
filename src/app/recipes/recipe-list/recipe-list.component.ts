bf90be25ac32c2b89bd9cc6ecf98eb49a87e8afa
Milad Fahimi <milad_0o0@yahoo.com>
2017-12-04 08:20:15 -0500

    Add first model to project. to recipe-list component. Also add some bootstrap class and ngFor for see my data on page.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+recipes: Recipe[] =[
+   new Recipe ('milad',
+  'this is a simple recipe',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTivP1llSFce8ZXlwKRnZrQwJozQtyRDwKfscSxcKMvkUaGZ1fY'),
+  new Recipe ('milad',
+  'this is a simple recipe',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTivP1llSFce8ZXlwKRnZrQwJozQtyRDwKfscSxcKMvkUaGZ1fY')
+];
 
   constructor() { }
 
