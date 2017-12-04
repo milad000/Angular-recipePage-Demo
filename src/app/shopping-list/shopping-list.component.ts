73384a18d85a84d97a6bd129a733a8fd94b567ea
Milad Fahimi <milad_0o0@yahoo.com>
2017-12-04 09:54:36 -0500

    Create a model for shopping-list try ngFor for see data in my page!

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/Ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingeridients: Ingredient [] = [
+  new Ingredient('Apple', 5),
+  new Ingredient('banana', 4)
+  ];
 
   constructor() { }
 
