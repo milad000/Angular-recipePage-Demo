ff929fb178d14615d9cb50454dd807f6aa744653
Milad Fahimi <milad_0o0@yahoo.com>
2017-12-05 15:04:06 -0500

    Add active class to navbar button by using ngClass.y

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit,Output, EventEmitter } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+itemSelected:string = 'recipe';
 @Output() featureSelected = new EventEmitter<string>();
 
 onSelect(feature:string){
   this.featureSelected.emit(feature);
+  if(feature == 'recipe'){
+    this.itemSelected = 'recipe';
+}else{
+    this.itemSelected = 'shopping-list';
+}
 }
   constructor() { }
 
