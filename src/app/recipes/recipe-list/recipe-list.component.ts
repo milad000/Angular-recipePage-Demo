56b77fab23174b3e6f509698cda7e797dcfb3916
Milad Fahimi <milad_0o0@yahoo.com>
2017-12-06 09:28:35 -0500

    Access to selected recipe array from nested component(detail-component).

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,18 +8,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes: Recipe[] =[
    new Recipe ('milad',
   'this is a simple recipe',
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTivP1llSFce8ZXlwKRnZrQwJozQtyRDwKfscSxcKMvkUaGZ1fY'),
   new Recipe ('milad',
   'this is a simple recipe',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTivP1llSFce8ZXlwKRnZrQwJozQtyRDwKfscSxcKMvkUaGZ1fY')
+  'https://www.underconsideration.com/brandnew/archives/firefox_2017_logo.jpg')
 ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedRecipe(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
